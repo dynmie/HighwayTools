9ef2e82fb5d17985d11e75ff6f8ff5aceae5a4a3
Dynmie <41315732+dynmie@users.noreply.github.com>
2024-06-29 19:30:05 -0700

    remove unneeded stuff

--- src/main/java/me/dynmie/highway/HighwayAddon.java
+++ src/main/java/me/dynmie/highway/HighwayAddon.java
@@ -1,23 +1,17 @@
 package me.dynmie.highway;
 
 import baritone.api.BaritoneAPI;
-import com.mojang.logging.LogUtils;
-import me.dynmie.highway.commands.ExampleCommand;
+import me.dynmie.highway.commands.CheckBlocksCommand;
 import me.dynmie.highway.highwaytools.pathing.BaritoneProcess;
-import me.dynmie.highway.hud.HudExample;
 import me.dynmie.highway.modules.HighwayTools;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import org.slf4j.Logger;
 
 public class HighwayAddon extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
+
     public static final Category CATEGORY = new Category("MetroHT");
-    public static final HudGroup HUD_GROUP = new HudGroup("MetroHT");
 
     @Override
     public void onInitialize() {
@@ -31,10 +25,7 @@ public class HighwayAddon extends MeteorAddon {
         BaritoneAPI.getProvider().getPrimaryBaritone().getPathingControlManager().registerProcess(new BaritoneProcess(tools));
 
         // Commands
-        Commands.add(new ExampleCommand());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        Commands.add(new CheckBlocksCommand(tools));
     }
 
     @Override
--- src/main/java/me/dynmie/highway/commands/CheckBlocksCommand.java
+++ src/main/java/me/dynmie/highway/commands/CheckBlocksCommand.java
@@ -4,22 +4,25 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import me.dynmie.highway.highwaytools.block.BlockTask;
 import me.dynmie.highway.modules.HighwayTools;
 import meteordevelopment.meteorclient.commands.Command;
-import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.command.CommandSource;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
-public class ExampleCommand extends Command {
+public class CheckBlocksCommand extends Command {
 
-    public ExampleCommand() {
-        super("checkblocks", "Sends a message.");
+    private final HighwayTools tools;
+
+    public CheckBlocksCommand(HighwayTools tools) {
+        super("checkblocks", "Check all block tasks.");
+
+        this.tools = tools;
     }
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            for (BlockTask value : Modules.get().get(HighwayTools.class).getTaskManager().getBlockTasks().values()) {
+            for (BlockTask value : tools.getTaskManager().getBlockTasks().values()) {
                 info(value.getBlockPos() + " " + value.getTaskState() + " " + MinecraftClient.getInstance().world.getBlockState(value.getBlockPos()).getBlock());
             }
             return SINGLE_SUCCESS;
