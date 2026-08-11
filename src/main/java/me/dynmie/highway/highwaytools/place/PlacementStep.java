9bba168bf4cf72bdc19019ba3799bd65e3b6ddf2
dynmie <41315732+dynmie@users.noreply.github.com>
2026-08-10 22:39:01 -0700

    fix: restore PlacementStep closing brace from blueprint-extras merge

--- src/main/java/me/dynmie/highway/highwaytools/place/PlacementStep.java
+++ src/main/java/me/dynmie/highway/highwaytools/place/PlacementStep.java
@@ -4,4 +4,4 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec3;
 
-public record PlacementStep(BlockPos supportPos, Direction side, Vec3 hitVec, BlockPos placedPos) {
+public record PlacementStep(BlockPos supportPos, Direction side, Vec3 hitVec, BlockPos placedPos) {}
