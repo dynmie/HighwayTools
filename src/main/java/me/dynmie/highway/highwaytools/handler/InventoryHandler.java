81dbbfaf697578de13b92390d2d16a07643f9553
dynmie <41315732+dynmie@users.noreply.github.com>
2026-08-10 19:27:35 -0700

    migrate: InventoryHandler to new enchantment API via meteor Utils/InvUtils

--- src/main/java/me/dynmie/highway/highwaytools/handler/InventoryHandler.java
+++ src/main/java/me/dynmie/highway/highwaytools/handler/InventoryHandler.java
@@ -1,19 +1,15 @@
 package me.dynmie.highway.highwaytools.handler;
 
 import me.dynmie.highway.modules.HighwayTools;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Objects;
 
@@ -24,7 +20,7 @@ import static meteordevelopment.meteorclient.MeteorClient.mc;
  */
 public class InventoryHandler {
 
-    private final MinecraftClient client = MinecraftClient.getInstance();
+    private final Minecraft client = Minecraft.getInstance();
     private final HighwayTools tools;
 
     private int waitTicks = 0;
@@ -74,7 +70,7 @@ public class InventoryHandler {
 
         FindItemResult hotbarResult = InvUtils.find(ItemStack::isEmpty, 0, 8);
 
-        int bestSlot = client.player.getInventory().selectedSlot + 1;
+        int bestSlot = client.player.getInventory().getSelectedSlot() + 1;
         if (bestSlot > 8) bestSlot = 0;
 
         if (hotbarResult.found() && !InvUtils.testInMainHand(ItemStack::isEmpty)) {
@@ -87,39 +83,28 @@ public class InventoryHandler {
     public FindItemResult findBestTool(BlockState state) {
         Objects.requireNonNull(mc.player, "player cannot be null");
 
-        Registry<Enchantment> enchantmentRegistry = mc.player.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT);
-
-        RegistryEntry.Reference<Enchantment> silkTouch = enchantmentRegistry.getOrThrow(Enchantments.SILK_TOUCH);
-
-        RegistryEntry.Reference<Enchantment> unbreaking = enchantmentRegistry.getOrThrow(Enchantments.UNBREAKING);
-        RegistryEntry.Reference<Enchantment> efficiency = enchantmentRegistry.getOrThrow(Enchantments.EFFICIENCY);
-        RegistryEntry.Reference<Enchantment> mending = enchantmentRegistry.getOrThrow(Enchantments.MENDING);
-        RegistryEntry.Reference<Enchantment> fortune = enchantmentRegistry.getOrThrow(Enchantments.FORTUNE);
-
         boolean noSilk = state.getBlock() == Blocks.ENDER_CHEST;
 
         double bestScore = 1;
         int slot = -1;
 
-        for (int i = 0; i < mc.player.getInventory().size(); i++) {
-            ItemStack stack = mc.player.getInventory().getStack(i);
+        for (int i = 0; i < mc.player.getInventory().getContainerSize(); i++) {
+            ItemStack stack = mc.player.getInventory().getItem(i);
 
-            if (!stack.isSuitableFor(state)) continue;
+            if (!stack.isCorrectToolForDrops(state)) continue;
 
-            if (EnchantmentHelper.getLevel(silkTouch, stack) != 0 && noSilk) {
+            if (noSilk && Utils.getEnchantmentLevel(stack, Enchantments.SILK_TOUCH) != 0) {
                 continue;
             }
 
-            double score = stack.getMiningSpeedMultiplier(state);
-
-            score += stack.getMiningSpeedMultiplier(state) * 1000;
-            score += EnchantmentHelper.getLevel(unbreaking, stack);
-            score += EnchantmentHelper.getLevel(efficiency, stack);
-            score += EnchantmentHelper.getLevel(mending, stack);
-            score += EnchantmentHelper.getLevel(fortune, stack);
+            double score = stack.getDestroySpeed(state) * 1001;
+            score += Utils.getEnchantmentLevel(stack, Enchantments.UNBREAKING);
+            score += Utils.getEnchantmentLevel(stack, Enchantments.EFFICIENCY);
+            score += Utils.getEnchantmentLevel(stack, Enchantments.MENDING);
+            score += Utils.getEnchantmentLevel(stack, Enchantments.FORTUNE);
 
             if (tools.getPreferSilkTouch().get()) {
-                score += EnchantmentHelper.getLevel(silkTouch, stack);
+                score += Utils.getEnchantmentLevel(stack, Enchantments.SILK_TOUCH);
             }
 
             if (score > bestScore) {
