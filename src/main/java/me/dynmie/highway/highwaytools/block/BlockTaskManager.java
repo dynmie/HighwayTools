f4b2a7da84db834f2359133157a0a1639cb689c1
dynmie <41315732+dynmie@users.noreply.github.com>
2026-08-10 22:38:56 -0700

    fix: add missing Blocks import from blueprint-extras merge

--- src/main/java/me/dynmie/highway/highwaytools/block/BlockTaskManager.java
+++ src/main/java/me/dynmie/highway/highwaytools/block/BlockTaskManager.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
